260d6320811f4466f24dc532fc729c28613aa6f4
Khalfan AlKindi <khalfan500@hotmail.com>
2025-03-01 23:06:18 +0300

    Configured MongoDB Atlas connection

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, "public")));
 
 // Connect to MongoDB
-mongoose.connect("mongodb://localhost:27017/instagram", {
+mongoose.connect("mongodb+srv://khalfandes:9-PAw4?qVHwxQXj@cluster0.0xwxe.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
